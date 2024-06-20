913b2230c4ece9c1a30dd5e0a544d76abd9e2893
HoangChien201 <hoangchien11522@gmail.com>
2024-06-20 12:01:18 +0700

    update message

--- src/message/entities/message.entity.ts
+++ src/message/entities/message.entity.ts
@@ -1,11 +1,11 @@
 import { GroupChat } from "src/group-chat/entities/group-chat.entity";
 import { LikeMessage } from "src/like-message/entities/like-message.entity";
 import { User } from "src/user/entities/user.entity";
-import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
 export class Message {
-    @PrimaryGeneratedColumn()
+    @PrimaryColumn()
     id:number
 
     @ManyToOne(()=>User,(user)=>user.id)
