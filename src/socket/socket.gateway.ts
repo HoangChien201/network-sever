151318f1533545f00f251ee086b6054c89657fac
Hoang Chien <130383912+HoangChien201@users.noreply.github.com>
2024-06-28 00:02:04 +0700

    Update socket.gateway.ts

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -17,10 +17,11 @@ export class SocketGateWay {
     @SubscribeMessage('message')
     async handleEvent(@MessageBody() messageSK: Message): Promise<void> {
         const memberOfGroup = await this.groupMemberRepository.find({ where: { group: typeof messageSK.group === 'number'? messageSK.group : messageSK.group.id} })
-
+        
+        
         if (!memberOfGroup) return
-
-        const memberOfGroupIDs = memberOfGroup.map(m => m.group).filter(id=>id !== messageSK.sender)
+        
+        const memberOfGroupIDs = memberOfGroup.map(m => m.user).filter(id=>id !== messageSK.sender)
         if (!memberOfGroupIDs) return
 
         memberOfGroupIDs.forEach((member) => {
