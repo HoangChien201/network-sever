f6b0e1c6646a95f06f55966ee0af21edeab3bccf
Hoang Chien <130383912+HoangChien201@users.noreply.github.com>
2024-08-24 13:43:20 +0700

    fix history

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,11 @@ import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Friendship } from 'src/friendship/entities/friendship.entity';
+import { LikeComment } from 'src/like-comment/entities/like-comment.entity';
+import { Comment } from 'src/comment/entities/comment.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([User,Friendship])],
+  imports:[TypeOrmModule.forFeature([User,Friendship,LikeComment,Comment])],
   controllers: [UserController],
   providers: [UserService],
   exports:[UserService]
